a70ee8444f78e1acdaf420488e44c84517894630
Pascal Martineau <pascal@lewebsimple.ca>
2025-12-19 14:43:02 -0500

    prune: remove ai

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,9 +25,6 @@ export default defineNuxtConfig({
         observability: {
           enabled: true,
         },
-        ai: {
-          binding: "AI",
-        },
         r2_buckets: [
           {
             binding: "BLOB",
