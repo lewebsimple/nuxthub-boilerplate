86b5534813a341e7f305e18f86cd4ab1d238d75c
Pascal Martineau <pascal@lewebsimple.ca>
2025-12-19 11:34:40 -0500

    fix: ai use cloudflare instance

--- server/api/ai.ts
+++ server/api/ai.ts
@@ -1,11 +1,12 @@
 import { generateText } from "ai";
+import { createWorkersAI } from "workers-ai-provider";
 
-export default defineEventHandler(async () => {
-  const workersai = getWorkersAI();
+export default defineEventHandler(async (event) => {
+  const { cloudflare } = event.context;
+  const workersai = createWorkersAI({ binding: cloudflare.env.AI });
   const model = workersai("@cf/meta/llama-3.1-8b-instruct", { safePrompt: true });
-
   const message = await generateText({
-    /** @ts-expect-error - AI SDK v6 breaking change */
+    /** @ts-expect-error - AI SDK v6 breaking change ? */
     model,
     prompt: "Write a humorous Hello World greeting.",
   });
--- server/utils/ai.ts
+++
@@ -1,8 +0,0 @@
-import { createWorkersAI } from "workers-ai-provider";
-
-let workersai: ReturnType<typeof createWorkersAI> | null = null;
-export function getWorkersAI() {
-  if (workersai) return workersai;
-  workersai = createWorkersAI({ binding: "AI" });
-  return workersai;
-}
